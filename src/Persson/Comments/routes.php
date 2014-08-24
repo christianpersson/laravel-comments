246778edc19ef75f275ab359f0a55aa0692a66b6
Christian Persson <christian-persson@live.se>
2014-08-24 14:03:43 +0200

    remove app specific route

--- src/Persson/Comments/routes.php
+++ src/Persson/Comments/routes.php
@@ -2,7 +2,3 @@
 Route::resource('api/comments', 'Persson\Comments\CommentsController',
     array('except' => array('create', 'edit')));
 
-Route::get('comments', function () {
-    JavaScript::put(['comment' => ['user_id' => Auth::id(), 'commentable_type' => 'Course', 'commentable_id' => 1]]);
-    return View::make('angular.commentsTest');
-});
